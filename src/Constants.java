abaa2b8d62e633b060654a45f522a9c9bc019742
Aditya <aj.adityajyoti@gmail.com>
2023-09-02 22:47:11 +0530

    Got rudimentary snake working

---
+++ src/Constants.java
@@ -0,0 +1,11 @@
+public class Constants {
+    private Constants() {
+    }
+
+    public static final int CELLS_IN_ROW = 25;
+    public static final int CELLS_IN_COL = 25;
+    public static final int CELL_SIZE = 30;
+
+    public static final int WIDTH = CELLS_IN_ROW * CELL_SIZE;
+    public static final int HEIGHT = CELLS_IN_COL * CELL_SIZE;
+}
